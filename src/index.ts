464e3863c7bc23b0536754d2e9eb2de3dced2c4d
YutakaHub <sdny36.ilr@icound.com>
2021-10-27 21:29:45 +0900

    first commiit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*import World from './world';
+
+const root = document.getElementById( "root" )
+const world = new World("Heloo World!");
+world.sayHello(root);
+*/
+
+import World from './world'
+
+const root: HTMLElement | null = document.getElementById('root')
+const world = new World('Hello Worldだよ!!!!')
+world.sayHello(root)
